3bc0cfe448535b1a05ce16168a02e2235f6866f3
youngers2006 <sy2224@ic.ac.uk>
2026-09-14 18:41:08 +0100

    Edited setup

--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@ from setuptools import setup, find_packages
 setup(
    name='iaif',
    version='1.0',
-   author='Markus Klar',
-   author_email='markus.klar@glasgow.ac.uk',
+   author='Markus Klar, Samuel Younger',
+   author_email='markus.klar@glasgow.ac.uk, Samuel.Younger24@imperial.ac.uk',
    packages=['iaif'],
    package_data={'': ['**']},
-   url='https://github.com/mkl4r/iaif',
+   url='https://github.com/youngers2006/iaif.git',
    license='LICENSE',
    python_requires='>=3.11',
    install_requires=[
-       "numpy", "optax", "tqdm", "pyyaml", "matplotlib", "pandas", "seaborn", "scikit-learn", "ipykernel", "difai @ git+https://github.com/mkl4r/difai-base.git@v1.1"
+       "numpy", "optax", "tqdm", "pyyaml", "matplotlib", "pandas", "seaborn", "scikit-learn", "ipykernel", "difai @ git+https://github.com/youngers2006/difai-base.git@main"
    ],
    extras_require={
     "gpu": ["jax[cuda13]"],
